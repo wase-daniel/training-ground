178cfe9fbb422a25f408b44537f1144bc86cf05b
wase-daniel <132266114+wase-daniel@users.noreply.github.com>
2026-04-14 05:15:09 -0700

    updating from using console to declare a variable

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 let firstCard = 10;
-let secondCard = 11;
-let sum = firstCard + secondCard + 4;
+let secondCard = 4;
+let sum = firstCard + secondCard;
 let hasBlackJack = false;
-// 1. Create a variable called isAlive and assign it to true
 let isAlive = true;
+// 1. Declare a variable called message and assign its value to an empty string
+let message = "";
 
-// 2. Flip its value to false in the appropriate code block
+// 2. Reassign the message variable to the string we're logging out
 if (sum <= 20) {
-  console.log("Do you want to draw a new card? 🙂");
+  message = "Do you want to draw a new card? 🙂";
 } else if (sum === 21) {
-  console.log("Wohoo! You've got Blackjack! 🥳");
+  message = "Wohoo! You've got Blackjack! 🥳";
   hasBlackJack = true;
 } else {
-  console.log("You're out of the game! 😭");
+  message = "You're out of the game! 😭";
   isAlive = false;
 }
 
-// 3. Log it out to check that you're doing it right
-console.log(isAlive);
+// 3. Log it out!
+console.log(message);
